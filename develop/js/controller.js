ee782e2f322edff442716b85e9c827875f49b7cd
Журавлев Павел Игоревич <zhuravlev@ek.apress.ru>
2016-06-30 17:02:29 +0500

    app did with promises

--- develop/js/controller.js
+++ develop/js/controller.js
@@ -1,46 +1,96 @@
 ;(function() {
   var
-    _offer = new Offer($('.js-grid')), 
-    _offerPopupSelector = '.js-popup',
     _tempPopUp = Handlebars.compile($('#offer-popup').html()),
+    _template = Handlebars.compile($('#offer').html()),
+    _offerPopupSelector = '.js-popup',
     _actionClass = '.js-parent-element',
     _offerSelector = '.js-grid-item',
-    _template = Handlebars.compile($('#offer').html()),
+    _offer = new Offer($('.js-grid')), 
     _user = new User({
       id: 10,
       name: 'Паша Журавлёв',
       foto: '../img/iam.jpg'
-    });
+    }),
+    _userOptions = {
+      'authorId': _user.id,
+      'name': _user.name,
+      'foto': _user.foto
+    },
+    _generalObj = {};
 
-  _offer.initOffers({
-    'authorId': _user.id,
-    'name': _user.name,
-    'foto': _user.foto
-  })
-  .then(function(data){
-    console.log(data);
+  _offer.initOffers(_userOptions)
+    .then(function(data) {
+      _generalObj.data = data;
+      _generalObj.user = _userOptions;
+      _offer.saveGeneralObj(_generalObj);
+
+      _reload(_template, _generalObj);
+    });
+  
+  $(document).on('click', '.js-delete-offer', function() {
+    var id = $(this).closest(_actionClass).data('id');
+   
+    _offer.delete(id)
+      .then(function(){
+        _reload(_template, _generalObj);
+      });
+   
+    return false;
   });
 
-  _startMansory($('.js-grid'), _offerSelector);
-  
-  $(document).on('click', '.js-call-popup', function() {
-    var id = $(this).closest(_actionClass).data('id');
-    
-    _renderPopUp(id);
+  $(document).on('click', '.js-delete-writing', function() {
+    var
+      $this = $(this), 
+      category = $this.data('category'),
+      idWriting = $this.closest('li').data('id'),
+      idOffer = $this.closest(_actionClass).data('id');
+
+    _offer.deleteWriting(idOffer, idWriting, category)
+      .then(function() {
+        _reload(_template, _generalObj);
+
+        if ($(_offerPopupSelector).length) {
+          _renderPopUp(idOffer);
+        }
+      });
 
     return false;
   });
 
-  $(document).mouseup(function(e) { 
-    var popUp = $('.offer-popup'); 
+  $(document).on('click', '.js-show-writings', function() {
+    var 
+      $this = $(this),
+      category = $this.data('category'),
+      idOffer = $this.closest(_actionClass).data('id');
 
-    if (popUp.has(e.target).length === 0) { 
-      _removePopUp(); 
-    }
+    $this.addClass('active');
+    _offer.showWritings(idOffer, category)
+      .then(function() {
+        _reload(_template, _generalObj);
+
+        if ($(_offerPopupSelector).length) {
+          _renderPopUp(idOffer);
+        }
+      });
+
+    return false;
   });
 
-  $(document).on('click', '.js-popup-close', function() {
-    _removePopUp();
+  $(document).on('click', '.js-add-prop', function() {
+    var
+      $this = $(this), 
+      idOffer = $this.closest(_actionClass).data('id'),
+      category = $this.data('category');
+
+    _offer.incrementCounter(idOffer, category)
+      .then(function(){
+        _reload(_template, _generalObj);
+
+        if ($(_offerPopupSelector).length) {
+          _renderPopUp(idOffer);
+        }
+      });
+
     return false;
   });
 
@@ -67,79 +117,48 @@
         return false;
       }
 
-      _offer.addWriting(id, userOptions, dataWriting);
+      _offer.addWriting(id, userOptions, dataWriting)
+        .then(function(){
+          _reload(_template, _generalObj);
 
-      if ($(_offerPopupSelector).length) {
-        _renderPopUp(id);
-      }
+          if ($(_offerPopupSelector).length) {
+            _renderPopUp(id);
+          }
+        });
 
       return false;
     }
   });
 
-  $(document).on('click', '.js-delete-offer', function() {
+  $(document).on('click', '.js-call-popup', function() {
     var id = $(this).closest(_actionClass).data('id');
-   
-    _offer.delete(id);
-   
+    
+    _renderPopUp(id);
+
     return false;
   });
 
-  $(document).on('click', '.js-delete-writing', function() {
-    var
-      $this = $(this), 
-      category = $this.data('category'),
-      idWriting = $this.closest('li').data('id'),
-      idOffer = $this.closest(_actionClass).data('id');
+  $(document).mouseup(function(e) { 
+    var popUp = $('.offer-popup'); 
 
-    _offer.deleteWriting(idOffer, idWriting, category);
-
-    if ($(_offerPopupSelector).length) {
-      _renderPopUp(idOffer);
+    if (popUp.has(e.target).length === 0) { 
+      _removePopUp(); 
     }
+  });
 
+  $(document).on('click', '.js-popup-close', function() {
+    _removePopUp();
     return false;
   });
 
-  $(document).on('click', '.js-show-writings', function() {
-    var 
-      $this = $(this),
-      category = $this.data('category'),
-      idOffer = $this.closest(_actionClass).data('id');
-
-    $this.addClass('active');
-    _offer.showWritings(idOffer, category);
-
-    if ($(_offerPopupSelector).length) {
-      _renderPopUp(idOffer);
-    }
-
-    return false;
-  });
-
-  $(document).on('click', '.js-add-prop', function() {
-    var
-      $this = $(this), 
-      idOffer = $this.closest(_actionClass).data('id'),
-      category = $this.data('category');
-
-    _offer.incrementCounter(idOffer, category);
-
-    if ($(_offerPopupSelector).length) {
-      _renderPopUp(idOffer);
-    }
-
-    _startMansory($('.js-grid'), _offerSelector);
-
-
-    return false;
-  });
-
-   // Рендеринг popup оффера(на вход id оффера)
+  // Рендеринг popup оффера(на вход id оффера)
   function _renderPopUp(id) {
     var 
       $container = $('body'),
-      popUpObj = _offer.getPopupData(id);
+      popUpObj = {
+        'activePopup': _generalObj.data.filter(function(item) { return item.id === id })[0],
+        'currentUser': _generalObj.user
+      };
 
     $(_offerPopupSelector).remove();
 
@@ -154,13 +173,50 @@
   }
 
   // Запускает masonry
-  function _startMansory($container, itemSelector) {
+  function _startMasonry($container, itemSelector) {
     $container.imagesLoaded(function() {
       $container.masonry({
         itemSelector: itemSelector,
         columnWidth: 235
       });
     });
+  }
+
+  // Проверяет, делал ли пользователь лайк или "добавить к себе"
+  function _checkAuthorField(generalObj, fields) {
+    var 
+      authorId = generalObj.user.authorId,
+      existingAuthors = [],
+      offers = generalObj.data,
+      dataLength = offers.length,
+      i, j, nameField, field;
+
+    for (j = 0; j < fields.length; j++) {
+      field = fields[j];
+      nameField = "show" + (field.charAt(0).toUpperCase() + field.substr(1));
+      
+      for (i = 0; i < dataLength; i++) {
+        existingAuthors = offers[i].offer[field].map(function(item) {
+          return item.authorId;
+        });
+          
+        if (existingAuthors.indexOf(authorId) != -1) {
+          offers[i].offer[nameField] = true;
+        } else {
+          offers[i].offer[nameField] = false;
+        }
+      }
+    }
+  }
+
+  // Перерисовывает контент
+  function _reload(template, data) {
+    var $container = $('.js-grid');
+
+    $('.container').html($container);  
+    _checkAuthorField(data, ["likes", "adding"]);
+    $container.html(template(data));
+    _startMasonry($container, _offerSelector);
   }
 
   // Сортировка данных по времени добавления
@@ -193,14 +249,4 @@
 
     return options.fn(result);
   });
-
-
-
-    // Перерисовывает контент
-  function _reload($container, template, data) {
-    $('.container').html($('.js-grid'));  
-    // _checkAuthorField(data, ["likes", "adding"]);
-    $container.html(template(data));
-    _startMansory($container, _offerSelector);
-  }
 })();
--- develop/js/offers.js
+++ develop/js/offers.js
@@ -1,8 +1,6 @@
 function Offer($container) {
   var 
     self = this,
-    _template = Handlebars.compile($('#offer').html()),
-    _offerSelector = '.js-grid-item',
     _generalObj = {};
 
   self.container = $container;
@@ -13,7 +11,7 @@ function Offer($container) {
     
     thisOffer.delete = true;
 
-    _updateData(self.container, _template, _generalObj);
+    return _sendData(_generalObj.data);
   }; 
 
   // Логическое удаление комментария/отзыва(определяется за счёт category) 
@@ -31,7 +29,38 @@ function Offer($container) {
 
     currentElement.show = false;
 
-    _updateData(self.container, _template, _generalObj); 
+    return _sendData(_generalObj.data);
+  };
+
+  // Показать комментарий/отзыв(определяется за счёт category)
+  self.showWritings = function(id, category) {
+    var offer = _findById(id, _generalObj.data).offer;
+    
+    if (category === "comments") {
+      offer.showComments = true;
+    } else if (category === "rewiews") {
+      offer.showRewiews = true;
+    }
+
+    return _sendData(_generalObj.data);
+  };
+
+  // Увеличение счётчика лайков/добавить к себе(определяется за счёт category)
+  self.incrementCounter = function(id, category) {
+    var 
+      offer = _findById(id, _generalObj.data).offer,    
+      existingAuthors = offer[category].map(function(item) { 
+        return item.authorId; 
+      }),
+      userOptions = $.extend(_generalObj.user, {'date': Date.now()});
+
+    if (existingAuthors.indexOf(userOptions.authorId) === -1) {
+      offer[category].push(userOptions);
+    } else {
+      offer[category].pop(userOptions);
+    }
+
+    return _sendData(_generalObj.data);
   };
 
   // Добавить комментарий/отзыв(определяется за счёт category) 
@@ -45,103 +74,37 @@ function Offer($container) {
         'show': true
       });
 
-      arrElements.push(elementField);
+    arrElements.push(elementField);
 
-      if (category === "comments") {
-        offer.commentsCount++;
-      } else if (category === "rewiews") {
-        offer.rewiewsCount++;
-      }
-
-      _updateData(self.container, _template, _generalObj);
-  };
-
-  // Показать комментарий/отзыв(определяется за счёт category)
-  self.showWritings = function(id, category) {
-    var offer = _findById(id, _generalObj.data).offer;
-    
     if (category === "comments") {
-      offer.showComments = true;
+      offer.commentsCount++;
     } else if (category === "rewiews") {
-      offer.showRewiews = true;
+      offer.rewiewsCount++;
     }
 
-    _updateData($container, _template, _generalObj);
+    return _sendData(_generalObj.data);
   };
 
-  // Увеличение счётчика лайков/добавить к себе(определяется за счёт category)
-  self.incrementCounter = function(id, category) {
-    var 
-      offer = _findById(id, _generalObj.data).offer,    
-      existingAuthors = _findByHash('authorId', offer[category]),
-      userOptions = $.extend(_generalObj.user, { 'date': Date.now() });
-
-    if (existingAuthors.indexOf(userOptions.authorId) === -1) {
-      offer[category].push(userOptions);
-    } else {
-      offer[category].pop(userOptions);
-    }
-    _updateData(self.container, _template, _generalObj);
-  };
-
-  // Отправить данные для попапа
-  self.getPopupData = function(id) {
-    return {
-      'activePopup': _findById(id, _generalObj.data),
-      'currentUser': _generalObj.user
-    };
+  // Копирует главный объект приложения, чтоб можно было
+  // работать с ним в этом классе
+  self.saveGeneralObj = function(obj) {
+    _generalObj = obj;
   };
 
   // Инициализация
-  self.initOffers = function(userOptions) {
-    return _getData(userOptions);
-  };
-
-  // Получает данные с сервера и отображает контент
-  function _getData(userOptions) {
+  self.initOffers = function() {
     return _httpGetData('/data');
-      // .then(function(data) {
-      //   _generalObj.data = data;
-      //   _generalObj.user = userOptions;
-        
-      //   _reload(self.container, _template, _generalObj);
-      // });
-  }
+  };
 
   // Отправляет данные на сервер
   function _sendData(data) {
-    $.post('/change', {'data': JSON.stringify(data, "", 2)});
-  }
-
-  // Обновление информации
-  function _updateData($container, template, generalObj) {
-    _reload($container, template, generalObj);
-    _sendData(generalObj.data);
-  }
-
-  // Проверяет, делал ли пользователь лайк или "добавить к себе"
-  function _checkAuthorField(generalObj, fields) {
-    var 
-      authorId = generalObj.user.authorId,
-      existingAuthors = [],
-      offers = generalObj.data,
-      dataLength = offers.length,
-      i, j, nameField, field;
-
-    for (j = 0; j < fields.length; j++) {
-      field = fields[j];
-      nameField = "show" + (field.charAt(0).toUpperCase() + field.substr(1));
-      
-      for (i = 0; i < dataLength; i++) {
-        existingAuthors = _findByHash('authorId', offers[i].offer[field]);
-          
-        if (existingAuthors.indexOf(authorId) != -1) {
-          offers[i].offer[nameField] = true;
-        } else {
-          offers[i].offer[nameField] = false;
-        }
-      }
-    }
+    return new Promise(function(resolve, reject) {
+      $.post(
+        '/change', 
+        {'data': JSON.stringify(data, "", 2)},
+        resolve()
+      );
+    });
   }
 
   // Поиск элемента массива по id  
@@ -158,17 +121,6 @@ function Offer($container) {
 
     return false;
   }
-
-  // Возвращает массив значений, хэш которых соответствует hash 
-  function _findByHash(hash, data) {
-    var arr = data.map(function(item) {
-      return item[hash];
-    });
-
-    return arr;
-  }
-
-
 
   // Получает данные(набор офферов) с сервера
   function _httpGetData(url) {
